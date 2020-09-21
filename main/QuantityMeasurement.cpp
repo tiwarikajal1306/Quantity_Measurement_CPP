5451cfc2f0751635491695e58f97a8ce3c8a3758
kajal <tiwarikajal1306@gmail.com>
2020-09-21 22:24:08 +0530

    ADDED Test for converting feet to inch

---
+++ main/QuantityMeasurement.cpp
@@ -0,0 +1,8 @@
+class QuantityMeasurement
+{
+public:    
+    double unit_converter(double value)
+    {
+        return value * 12;
+    }
+};
--- test/feetTest.cpp
+++ test/feetTest.cpp
@@ -1,5 +1,12 @@
 #include "../main/feet.h"
 #include <gtest/gtest.h>
+#include "../main/QuantityMeasurement.cpp"
+
+TEST(LengthComparisionTest, _one_feet_and_twelve_inch__should_be_equal)
+{
+    QuantityMeasurement quantity;
+    ASSERT_EQ(12, quantity.unit_converter(1));
+}
 
 int main(int argc, char **argv)
 {
