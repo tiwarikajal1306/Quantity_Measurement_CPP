269f867400978d9d96bbfb3a7117fee6c88c7700
kajal <tiwarikajal1306@gmail.com>
2020-09-24 02:06:53 +0530

    ADDED Test case for addition of feet values

--- test/quantity_measurement_test.cpp
+++ test/quantity_measurement_test.cpp
@@ -149,6 +149,16 @@ TEST(LengthComparisionTest, _addition_of_one_feet_and_two_inch__should_be_fourte
     quantity_measurement measurement(&first_inch, &second_inch);
     ASSERT_EQ(14.0, measurement.add_quantity(unit::INCH.value));
 }
+
+
+TEST(LengthComparisionTest, _addition_of_one_feet_and_one_feet__should_be_twentyfour_inch)
+{
+    quantity first_feet(1.0, unit::FEET);
+    quantity second_feet(1.0, unit::FEET);
+    quantity_measurement measurement(&first_feet, &second_feet);
+    ASSERT_EQ(24.0, measurement.add_quantity(unit::INCH.value));
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
