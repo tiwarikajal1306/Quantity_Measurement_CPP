3e85cc968fd82509f51918f55160b185b08edd16
kajal <tiwarikajal1306@gmail.com>
2020-09-24 23:18:48 +0530

    REFACTORED Code

--- main/quantity.cpp
+++ main/quantity.cpp
@@ -8,6 +8,7 @@ public:
 
 public:
     quantity(double value, unit unit_type) : unit_type(unit_type), value(value * (unit_type.value)) {}
+    quantity() {}
 
     bool operator==(quantity other) const
     {
@@ -16,31 +17,45 @@ public:
             return (this->value == other.value);
         }
 
+        if (this->unit_type.type == other.unit_type.type && this->value == other.value)
+        {
+            return (this->value == other.value);
+        }
         return false;
     }
 
     bool operator==(quantity *other) const
     {
-        if (this == nullptr && other == nullptr)
+        if (other == nullptr)
         {
             return false;
         }
 
-        if (this->unit_type.type == other->unit_type.type)
-        {
-            return (this->value == other->value);
-        }
-
-        return false;
+        return (*this == *other);
     }
 
-     bool operator!=(quantity other) const
+    bool operator!=(quantity other) const
     {
-        if (this->unit_type.type == other.unit_type.type && this->unit_type.value == other.unit_type.value)
-        {
-            return (this->value == other.value);
-        }
-
-        return (this->unit_type.type == other.unit_type.type);
+        return !(*this == other);
     }
+
+    quantity operator+(quantity other) const
+    {
+        quantity result_quantity;
+
+        double added_value = this->value + other.value;
+
+        result_quantity.value = added_value;
+
+        result_quantity.unit_type = this->unit_type;
+
+        return result_quantity;
+    }
+quantity convert_celsius_to_fahrenheit()
+{
+    quantity result_quantity;
+    result_quantity.value = value * 1.8 + 32;
+    result_quantity.unit_type = unit::FAHRENHITE;
+    return result_quantity;
+}
 };
--- test/quantity_measurement_test.cpp
+++ test/quantity_measurement_test.cpp
@@ -1,169 +1,154 @@
 #include <gtest/gtest.h>
-#include "../main/quantity_measurement.h"
+#include "../main/quantity.cpp"
 
 TEST(LengthComparisionTest, _one_feet_and_twelve_inch_should_be_equal)
 {
-    quantity quantity1(1.0, unit::FEET);
-    quantity quantity2(12.0, unit::INCH);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity one_feet(1.0, unit::FEET);
+    quantity twelve_inch(12.0, unit::INCH);
+    ASSERT_EQ(one_feet, twelve_inch);
 }
+
 //TC 1.1
 TEST(LengthComparisionTest, _zero_feet_and_zero_feet__should_be_equal)
 {
-    quantity quantity1(0.0, unit::FEET);
-    quantity quantity2(0.0, unit::FEET);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity first_zero_feet(0.0, unit::FEET);
+    quantity second_zero_feet(0.0, unit::FEET);
+    ASSERT_EQ(first_zero_feet, second_zero_feet);
 }
 
 //TC 1.2
 TEST(LengthComparisionTest, _null_feet_and_one_feet__should_not_be_equal)
 {
-    quantity quantity1(1.0, unit::FEET);
-    ASSERT_NE(&quantity1, nullptr);
+    quantity one_feet(1.0, unit::FEET);
+    ASSERT_NE(&one_feet, nullptr);
 }
 
 //TC 1.3
 TEST(LengthComparisionTest, __two_refrance_point_to_same_address_should_be_equal)
 {
-    quantity *quantity1 = new quantity(1.0, unit::FEET);
-    quantity *quantity2 = quantity1;
-    ASSERT_EQ(quantity1, quantity2);
-}
-
-//TC 1.4
-TEST(LengthComparisionTest, __first_refrance_and_second_refrance_should_not_be_equal)
-{
-    quantity *quantity1 = new quantity(1.0, unit::FEET);
-    quantity *quantity2 = new quantity(1.0, unit::FEET);
-    ASSERT_NE(quantity1, quantity2);
+    quantity *one_feet = new quantity(1.0, unit::FEET);
+    quantity *second_value = one_feet;
+    ASSERT_EQ(one_feet, second_value);
 }
 
 //TC 1.5
 TEST(LengthComparisionTest, _two_different_type_of_quantity_should_not_be_equal)
 {
-    quantity quantity1(1.0, unit::FEET);
-    quantity quantity2(1.0, unit::INCH);
-    ASSERT_NE(quantity1, quantity2);
+    quantity one_feet(1.0, unit::FEET);
+    quantity one_inch(1.0, unit::INCH);
+    ASSERT_NE(one_feet, one_inch);
 }
 
 //TC 1.6
 TEST(LengthComparisionTest, _two_same_type_of_quantity_should_be_equal)
 {
-    quantity quantity1(1.0, unit::FEET);
-    quantity quantity2(1.0, unit::FEET);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity first_one_feet(1.0, unit::FEET);
+    quantity second_one_feet(1.0, unit::FEET);
+    ASSERT_EQ(first_one_feet, second_one_feet);
 }
 
 //TC 1.7
 TEST(LengthComparisionTest, _null_inch_and_one_inch__should_not_be_equal)
 {
-    quantity quantity1(1.0, unit::INCH);
-    ASSERT_NE(&quantity1, nullptr);
+    quantity one_inch(1.0, unit::INCH);
+    ASSERT_NE(&one_inch, nullptr);
 }
 
 //TC 1.8
 TEST(LengthComparisionTest, _first_inch_refrarnce_and_second_refrance_should_be_equal)
 {
-    quantity *quantity1 = new quantity(1.0, unit::INCH);
-    quantity *quantity2 = quantity1;
-    ASSERT_EQ(quantity1, quantity2);
-}
-
-//TC 1.9
-TEST(LengthComparisionTest, _first_inch_refrance_and_second_inch_refrance_should_not_be_equal)
-{
-    quantity *quantity1 = new quantity(1.0, unit::FEET);
-    quantity *quantity2 = new quantity(1.0, unit::FEET);
-    ASSERT_NE(quantity1, quantity2);
+    quantity *one_feet = new quantity(1.0, unit::INCH);
+    quantity *second_value = one_feet;
+    ASSERT_EQ(one_feet, second_value);
 }
 
 //TC 1.10
 TEST(LengthComparisionTest, _two_same_inch_type_of_quantity_should_be_equal)
 {
-    quantity quantity1(1.0, unit::INCH);
-    quantity quantity2(1.0, unit::INCH);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity first_one_inch(1.0, unit::INCH);
+    quantity second_one_inch(1.0, unit::INCH);
+    ASSERT_EQ(first_one_inch, second_one_inch);
 }
 
 //TC 1.11
 TEST(LengthComparisionTest, _zero_inch_and_zero_inch__should_be_equal)
 {
-    quantity quantity1(0.0, unit::INCH);
-    quantity quantity2(0.0, unit::INCH);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity first_zero_inch(0.0, unit::INCH);
+    quantity second_zero_inch(0.0, unit::INCH);
+    ASSERT_EQ(first_zero_inch, second_zero_inch);
 }
 
 //UC2
 TEST(LengthComparisionTest, _three_feet_and_one_yard_should_be_equal)
 {
-    quantity quantity1(3.0, unit::FEET);
-    quantity quantity2(1.0, unit::YARD);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity three_feet(3.0, unit::FEET);
+    quantity one_yard(1.0, unit::YARD);
+    ASSERT_EQ(three_feet, one_yard);
 }
 
 //TC 1.13
 TEST(LengthComparisionTest, _one_feet_and_one_yard__should_not_be_equal)
 {
-    quantity quantity1(1.0, unit::FEET);
-    quantity quantity2(1.0, unit::YARD);
-    ASSERT_NE(quantity1, quantity2);
+    quantity one_feet(1.0, unit::FEET);
+    quantity one_yard(1.0, unit::YARD);
+    ASSERT_NE(one_feet, one_yard);
 }
 
 //TC 1.14
 TEST(LengthComparisionTest, _one_inch_and_one_yard__should_not_be_equal)
 {
-    quantity quantity1(1.0, unit::INCH);
-    quantity quantity2(1.0, unit::YARD);
-    ASSERT_NE(quantity1, quantity2);
+    quantity one_inch(1.0, unit::INCH);
+    quantity one_yard(1.0, unit::YARD);
+    ASSERT_NE(one_inch, one_yard);
 }
 
 //TC 1.15
 TEST(LengthComparisionTest, _one_yard_and_thirtysix_inch_should_be_equal)
 {
-    quantity quantity1(1.0, unit::YARD);
-    quantity quantity2(36.0, unit::INCH);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity one_yard(1.0, unit::YARD);
+    quantity thirtysix_inch(36.0, unit::INCH);
+    ASSERT_EQ(one_yard, thirtysix_inch);
 }
 
 //UC 3
 TEST(LengthComparisionTest, _two_inch_and_five_cm_should_be_equal)
 {
-    quantity quantity1(2.0, unit::INCH);
-    quantity quantity2(5.0, unit::CM);
-    ASSERT_EQ(quantity1, quantity2);
+    quantity two_inch(2.0, unit::INCH);
+    quantity five_cm(5.0, unit::CM);
+    ASSERT_EQ(two_inch, five_cm);
 }
 
 //UC 4
 TEST(LengthComparisionTest, _addition_of_two_inch_and_two_inch__should_be_four_inch)
 {
-    quantity first_inch(2.0, unit::INCH);
-    quantity second_inch(2.0, unit::INCH);
-    quantity_measurement measurement(&first_inch, &second_inch);
-    ASSERT_EQ(4.0, measurement.add_quantity(unit::INCH.value));
+    quantity first_two_inch(2.0, unit::INCH);
+    quantity second_two_inch(2.0, unit::INCH);
+    quantity four_inch(4.0, unit::INCH);
+    ASSERT_EQ(four_inch, first_two_inch + second_two_inch);
 }
 
 TEST(LengthComparisionTest, _addition_of_one_feet_and_two_inch__should_be_fourteen_inch)
 {
-    quantity first_inch(1.0, unit::FEET);
-    quantity second_inch(2.0, unit::INCH);
-    quantity_measurement measurement(&first_inch, &second_inch);
-    ASSERT_EQ(14.0, measurement.add_quantity(unit::INCH.value));
+    quantity one_feet(1.0, unit::FEET);
+    quantity two_inch(2.0, unit::INCH);
+    quantity fourteen_inch(14.0, unit::INCH);
+    ASSERT_EQ(fourteen_inch, one_feet + two_inch);
 }
 
 TEST(LengthComparisionTest, _addition_of_one_feet_and_one_feet__should_be_twentyfour_inch)
 {
-    quantity first_feet(1.0, unit::FEET);
-    quantity second_feet(1.0, unit::FEET);
-    quantity_measurement measurement(&first_feet, &second_feet);
-    ASSERT_EQ(24.0, measurement.add_quantity(unit::INCH.value));
+    quantity first_one_feet(1.0, unit::FEET);
+    quantity second_one_feet(1.0, unit::FEET);
+    quantity twenty_four_inch(24.0, unit::INCH);
+    ASSERT_EQ(twenty_four_inch, first_one_feet + second_one_feet);
 }
 
 TEST(LengthComparisionTest, _addition_of_two_inch_and_two_and_half_cm__should_be_three_inch)
 {
-    quantity first_inch(2.0, unit::INCH);
-    quantity second_cm(2.5, unit::CM);
-    quantity_measurement measurement(&first_inch, &second_cm);
-    ASSERT_EQ(3.0, measurement.add_quantity(unit::INCH.value));
+    quantity two_inch(2.0, unit::INCH);
+    quantity two_and_half_cm(2.5, unit::CM);
+    quantity three_inch(3.0, unit::INCH);
+    ASSERT_EQ(three_inch, two_inch + two_and_half_cm);
 }
 
 //UC 5
@@ -185,18 +170,18 @@ TEST(VolumeComparisionTest, _one_litre_and_thousand_ml_should_be_equal)
 TEST(VolumeComparisionTest,
      _addition_of_one_gallon_and_three_point_seven_eight_litre_should_be_seven_point_five_six_litre)
 {
-    quantity gallon(1.0, unit::GALLON);
-    quantity litre(3.78, unit::LITRE);
-    quantity_measurement measurement(&gallon, &litre);
-    ASSERT_EQ(7.56, measurement.add_quantity(unit::LITRE.value));
+    quantity one_gallon(1.0, unit::GALLON);
+    quantity three_point_78_litre(3.78, unit::LITRE);
+    quantity seven_point_56_litere(7.56, unit::LITRE);
+    ASSERT_EQ(seven_point_56_litere, one_gallon + three_point_78_litre);
 }
 
 TEST(VolumeComparisionTest, _addition_of_one_litre_and_thousand_ml_should_be_two_litre)
 {
-    quantity litre(1.0, unit::LITRE);
-    quantity ml(1000.0, unit::ML);
-    quantity_measurement measurement(&litre, &ml);
-    ASSERT_EQ(2.0, measurement.add_quantity(unit::LITRE.value));
+    quantity one_litre(1.0, unit::LITRE);
+    quantity thousand_ml(1000.0, unit::ML);
+    quantity two_litres(2.0, unit::LITRE);
+    ASSERT_EQ(two_litres, one_litre + thousand_ml);
 }
 
 //UC7
@@ -216,10 +201,10 @@ TEST(WeightComparisionTest, _one_tonne_and_thousand_kg_should_be_equal)
 
 TEST(WeightComparisionTest, _addition_of_one_tonne_and_thousand_kg_should_be_thousand_one_kg)
 {
-    quantity tonne(1.0, unit::TONNE);
-    quantity kg(1.0, unit::KG);
-    quantity_measurement measurement(&tonne, &kg);
-    ASSERT_EQ(1001.0, measurement.add_quantity(unit::KG.value));
+    quantity one_tonne(1.0, unit::TONNE);
+    quantity one_kg(1.0, unit::KG);
+    quantity one_thousand_one_kg(1001.0, unit::KG);
+    ASSERT_EQ(one_thousand_one_kg, one_tonne + one_kg);
 }
 
 //UC 8
@@ -227,8 +212,7 @@ TEST(TemperatureComparisionTest, _212F_and_100C_should_be_equal)
 {
     quantity fahrenheit(212.0, unit::FAHRENHITE);
     quantity celsius(100.0, unit::CELSIUS);
-    quantity_measurement measurement;
-    ASSERT_EQ(fahrenheit.value, measurement.convert_celsius_to_fahrenheit(celsius.value));
+    ASSERT_EQ(fahrenheit, celsius.convert_celsius_to_fahrenheit());
 }
 
 int main(int argc, char **argv)
